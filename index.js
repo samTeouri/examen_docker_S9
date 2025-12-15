db20f1bc7d795e9071b33dc4dfa03c4496d0f75f
Samrou Teouri <teourisamrou@gmail.com>
2025-12-15 10:35:23 +0100

    feat: add dotenv configuration and database connection

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+import * as database from './database.js';
+
+database.connect();
+
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,11 +15,11 @@ app.use(express.static('public'));
 
 // Sample route
 app.post('/plus', (req, res) => {
-  res.send('Hello, World!');
+  
 });
 
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Serveur disponible sur l'adresse suivante : http://localhost:${PORT}`);
 });
