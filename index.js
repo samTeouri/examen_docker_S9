83a686f4a8433031f0e0811583268a4cec95eb59
Samrou Teouri <teourisamrou@gmail.com>
2025-12-15 10:19:34 +0100

    feat : add static file serving and create initial index.html

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
