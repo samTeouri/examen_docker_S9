6b1735d439d6427b0dbbbd6fd7fd258ec41ba318
Samrou Teouri <teourisamrou@gmail.com>
2025-12-15 10:24:23 +0100

    feat: implement static file serving and add initial HTML structure

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 // Sample route
-app.get('/', (req, res) => {
+app.post('/plus', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
 
 // Start the server
 app.listen(PORT, () => {
